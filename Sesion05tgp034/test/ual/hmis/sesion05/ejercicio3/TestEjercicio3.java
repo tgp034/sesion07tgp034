2720b6de59367d43c179efd634674e966f18d53f
Tito Julio González Padial <72378065+tgp034@users.noreply.github.com>
2022-06-01 12:30:42 +0200

    Add files via upload

--- Sesion05tgp034/test/ual/hmis/sesion05/ejercicio3/TestEjercicio3.java
+++ Sesion05tgp034/test/ual/hmis/sesion05/ejercicio3/TestEjercicio3.java
@@ -1,7 +1,6 @@
 package ual.hmis.sesion05.ejercicio3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
--- Sesion05tgp034/test/ual/hmis/sesion05/ejercicio4/TestEjercicio4.java
+++ Sesion05tgp034/test/ual/hmis/sesion05/ejercicio4/TestEjercicio4.java
@@ -1,35 +1,25 @@
 package ual.hmis.sesion05.ejercicio4;
 
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.ArrayList;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 import ual.hmis.sesion05.ejercicio4.Ejercicio4;
 
-class TestEjercicio4 {
+public class TestEjercicio4 {
+	
+	// Arrange
+	Ejercicio4 e4 = new Ejercicio4();
+	
+	@ParameterizedTest
+	@CsvSource({ "-10,Error en la nota", "0,Suspenso", "5,Aprobado", "7,Notable", "9,Sobresaliente", "10,Matricula",
+			"11,Error en la nota" })
+	void testCalificaciones(Float nota, String expected) {
 
-		@ParameterizedTest (name = "{index} => calificacion")
-		@CsvSource({"-10,Error en la nota","0,Suspenso","5,Aprobado",
-		"7,Notable", "9,Sobresaliente","10,Matricula","11,Error en la nota"})
-		void testCalificacion(Float nota, String expected) {
-			//Arrange
-			Ejercicio4 e4 = new Ejercicio4();
-			//Act
-			String calificacion = e4.calificacion(nota);
-			//Assert
-			assertEquals(expected,calificacion);
-		}
-		
-//		@ParameterizedTest (name = "{index} => calificaciones")
-//		@CsvSource({"[-1;4.9;5.9;8.9;9.9;10;654]",
-//				 "[Error en la nota; Suspenso; Aprobado; Notable; Sobresaliente; Matricula; Error en la nota]"})
-//		void testCalificaciones(ArrayList<Float> notas, ArrayList<String> expected) {
-//			//Arrange
-//			Ejercicio4 e4 = new Ejercicio4();
-//			//Act
-//			e4.calculaCalificaciones(notas);
-//			//Assert
-//			assertEquals(expected,e4.calificaciones);
-//		}
+		// Act
+		e4.notas.add(nota);
+		e4.calculaCalificaciones(e4.notas);
+		// Assert
+		assertEquals(expected, e4.calificaciones.get(0));
 	}
+}
